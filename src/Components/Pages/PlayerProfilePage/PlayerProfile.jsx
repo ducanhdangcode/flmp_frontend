61725cff5b232e4d91fad7d3992ecf2a34f2624c
ducanhdangcode <hayatokun81024@gmail.com>
2025-02-26 23:17:23 +0700

    Add player profile header

--- src/Components/Pages/PlayerProfilePage/PlayerProfile.jsx
+++ src/Components/Pages/PlayerProfilePage/PlayerProfile.jsx
@@ -33,11 +33,35 @@ const PlayerProfile = () => {
         <div className = "relative left-[2.5rem] top-[1.2rem]">
           {/* name */}
           <p className = "font-alfa font-bold text-2xl">{player?.name}</p>
-          <div className = "flex relative top-[0.5rem]">
+          <div className = "flex relative top-[0.5rem] bg-[#ededed] h-[12.5rem] right-[1rem] w-[35vw] rounded-[5px]">
             {/* first col */}
-            <div className = "font-ubuntu">
+            <div className = "font-ubuntu relative top-[1.5rem] left-[1rem]">
               {/* date of birth */}
               <p>Date of birth: <span className = "font-bold">{player?.birthDate}</span></p>
+              {/* height */}
+              <p className = "relative top-[1rem]">Height: <b>{`${player?.height}cm`}</b></p>
+              {/* birth place */}
+              <div className = "flex relative top-[2rem]">
+                <p className = "">Place of birth: <b>{player?.birthPlace}</b></p>
+                <img src = {player?.nationalityImageLink} alt = "" className = "w-[1.2rem] h-[1.2rem] relative left-[0.5rem] top-[0.1rem]" />
+              </div>
+              {/* nationality */}
+              <div className = "flex relative top-[3rem]">
+                <p>Nationality: <b>{player?.nationality}</b></p>
+                <img src = {player?.nationalityImageLink} alt = "" className = "w-[1.2rem] h-[1.2rem] relative left-[0.5rem] top-[0.1rem]" />
+              </div>
+            </div>
+
+            {/* second col */}
+            <div className = "font-ubuntu relative top-[1.5rem] left-[5rem]">
+              {/* position */}
+              <p>Position: <b>{player?.detailPosition}</b></p>
+              {/* other position */}
+              <p className = "relative top-[1rem]">Other position: <b>{player?.otherPosition}</b></p>
+              {/* preferred foot */}
+              <p className = "relative top-[2rem]">Preferred Foot: <b>{player?.preferredFoot}</b></p>
+              {/* former international */}
+              <p className = "relative top-[3rem]">Former International: <b>{player?.formerInternational}</b></p>
             </div>
           </div>
         </div>
